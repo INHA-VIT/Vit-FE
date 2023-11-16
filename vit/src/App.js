76b9e1b66f82ebda81a4c5d86039efb411dd84db
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-16 17:33:32 +0900

    Feat: 보유 지역 카드 목록 페이지 생성

--- vit/src/App.js
+++ vit/src/App.js
@@ -1,6 +1,7 @@
 import Intro from "./pages/Intro";
 import PickStamp from "./pages/PickStamp";
 import DetailPage from "./pages/DetailPage";
+import WalletPage from "./pages/WalletPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Intro />} />
         <Route path="/pickStamp" element={<PickStamp />} />
         <Route path="/detailPage" element={<DetailPage />} />
+        <Route path="/walletPage" element={<WalletPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ vit/src/pages/WalletPage.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Slider from "react-slick";
+import gangneungCard from "../assets/gangneungCard.png";
+import busanCard from "../assets/busanCard.png";
+import jejuCard from "../assets/jejuCard.png";
+import jeonjuCard from "../assets/jeonjuCard.png";
+import sokchoCard from "../assets/sokchoCard.png";
+import styled from "styled-components";
+import { IoChevronBack } from "react-icons/io5";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Body = styled.div`
+  width: 380px;
+  height: 840px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap");
+  font-family: "Noto Sans KR", sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+  width: 100vh;
+  height: 50px;
+  margin-left: 15px;
+  margin-top: 55px;
+`;
+
+const StyledSlider = styled(Slider)`
+  padding-left: 23px;
+  .slick-dots {
+    left: 5px;
+  }
+`;
+
+const CardCover = styled.img`
+  width: 330px;
+  height: 620px;
+  object-fit: contain;
+`;
+
+const WalletPage = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    customPaging: function (i) {
+      return (
+        <div
+          style={{
+            width: "5px",
+            height: "5px",
+            background: "#C0C0C0",
+          }}
+        />
+      );
+    },
+  };
+  return (
+    <Body>
+      <ButtonWrapper>
+        <IoChevronBack />
+        &nbsp;&nbsp;지역 발급 카드 관리
+      </ButtonWrapper>
+      <StyledSlider {...settings}>
+        <div>
+          <CardCover src={gangneungCard} />
+        </div>
+        <div>
+          <CardCover src={busanCard} />
+        </div>
+        <div>
+          <CardCover src={jejuCard} />
+        </div>
+        <div>
+          <CardCover src={jeonjuCard} />
+        </div>
+        <div>
+          <CardCover src={sokchoCard} />
+        </div>
+      </StyledSlider>
+    </Body>
+  );
+};
+
+export default WalletPage;
