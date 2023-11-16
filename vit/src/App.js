e383ab553326fd2313c8e0b3ebb8788e6d8c821e
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-16 15:03:01 +0900

    Chore: 라우터 연결

--- vit/src/App.js
+++ vit/src/App.js
@@ -1,12 +1,17 @@
 import Intro from "./pages/Intro";
 import PickStamp from "./pages/PickStamp";
 import DetailPage from "./pages/DetailPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <DetailPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/pickStamp" element={<PickStamp />} />
+        <Route path="/detailPage" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
